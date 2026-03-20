24daa75efcdde32de34e541c7187f29c63f4b776
erik-mauricio <em882@cornell.edu>
2026-03-20 00:46:11 -0400

    added merge postings and inverted

--- src/analysis.py
+++ src/analysis.py
@@ -1,2 +1,18 @@
 from text_preprocess import tokenize
 
+def merge_postings(term1,term2):
+    postings1=inverted_index[term1]
+    postings2=inverted_index[term2]
+    merged_posting=[]
+    i,j=0,0
+    while i<len(postings1) and j<len(postings2):
+        if postings1[i]==postings2[j]:
+          merged_posting.append(postings1[i])
+          i+=1
+          j+=1
+        elif postings1[i]<postings2[j]:
+          i+=1
+        else:
+          j+=1
+
+    return merged_posting
